4401a4044f2c8e4ca9efbbcd3ecdb91e51c6aef3
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-02-01 13:26:39 +0200

    practic module 4

--- src/interface.ts
+++ src/interface.ts
@@ -5,6 +5,64 @@ interface IPerson{
     greet(phrase:string): void;
 }
 
+interface IPilot{
+    flyMessage(): void;
+}
+
+class Pilot implements IPerson, IPilot{
+    constructor(public name: string, public age: number) {
+        this.checkAge();
+    }
+
+    private checkAge() {
+        if (this.age < 28) {
+           throw new Error("pilot to young")
+       } 
+    }
+   public greet(phrase: string):void {
+        console.log(phrase + " "+ this.name)
+    }
+    public flyMessage(): void {
+        console.log("Have a nice flight!")
+    }
+}
+
+
+abstract class Plane{
+    protected pilot?: IPilot;
+
+    public sitInPlane(pilot:IPilot) {
+        this.pilot = pilot;
+    }
+
+    public abstract startEngine(): boolean;
+}
+
+class Airbus extends Plane{
+    public startEngine() {
+        if (!this.pilot) {
+            throw new Error("No pilot in cabin")
+        }
+        console.log("Engine srart");
+        this.pilot.flyMessage();
+        return true
+    }
+}
+
+
+const pilot = new Pilot("Jack", 35);
+const airbus = new Airbus();
+pilot.greet("hello i'm a pilot");
+airbus.sitInPlane(pilot);
+airbus.startEngine();
+
+
+
+
+
+
+
+
 let user: IPerson;
 user = {
     name: "Fusun",
@@ -16,6 +74,8 @@ user = {
 }
 
 user.greet("Welcome , My name is ")
+
+
 
 type IPersonType = {
     readonly name: string;
--- src/plane.ts
+++ src/plane.ts
@@ -1,32 +1,32 @@
 
-abstract class Plane{
-    protected pilotInCabin = false;
+// abstract class Plane{
+//     protected pilotInCabin = false;
 
-    public sitInPlane() {
-        this.pilotInCabin = true;
-    }
+//     public sitInPlane() {
+//         this.pilotInCabin = true;
+//     }
 
-    public abstract startEngine(): string;
-}
+//     public abstract startEngine(): string;
+// }
 
-class Airbus extends Plane{
-    public startEngine() {
-        return "YYYYY"
-    }
-}
+// class Airbus extends Plane{
+//     public startEngine() {
+//         return "YYYYY"
+//     }
+// }
 
-class Boing extends Plane{
-    public startEngine() {
-        return "wwwww"
-    }
-}
+// class Boing extends Plane{
+//     public startEngine() {
+//         return "wwwww"
+//     }
+// }
 
-const airbus320 = new Airbus();
-const boing737 = new Boing();
+// const airbus320 = new Airbus();
+// const boing737 = new Boing();
 
-airbus320.sitInPlane();
-boing737.sitInPlane();
+// airbus320.sitInPlane();
+// boing737.sitInPlane();
 
-console.log(airbus320.startEngine());
-console.log(boing737.startEngine());
+// console.log(airbus320.startEngine());
+// console.log(boing737.startEngine());
 
